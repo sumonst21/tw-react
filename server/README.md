7cdbe95d6a49f4b824d6cf53170c38f70bd14d23
Tim Wheeler <tim@timwheeler.com>
2018-08-28 00:49:16 -0400

    edited README.md

--- server/README.md
+++ server/README.md
@@ -2,8 +2,15 @@
 
 ##### Technologies Used
 * React
+* MongoDB
+* Mongoose
+* Express
+* Node.js
 * Styled Components
 * Typeit.js
+* Axios
+* Babel
+* Webpack
 
 
 Using React to rebuild TimWheeler.com
--- server/index.js
+++ server/index.js
@@ -36,10 +36,23 @@ app.post('/thank-you', (req, res) => {
     });
 });
 
-app.get('/thank-you', (req, res) => {
-  res.send('Thank You');
-});
+// app.get('/thank-you', (req, res) => {
+//   res.send('Thank You');
+// });
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // Like main.js or main.css
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html
+  // If it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = 5000;
 app.listen(PORT, () => {
